69834da236bef62e0b7e1e2dd59d548901452687
Momen Yosri <momen.yosri@gmail.com>
2025-01-29 20:36:28 +0200

    Refactor RoomModel to use non-nullable fields; implement real-time message streaming in ChatViewModel; enhance ChatScreen to display messages dynamically

--- lib/core/model/room_model.dart
+++ lib/core/model/room_model.dart
@@ -1,8 +1,8 @@
 class RoomModel {
   static const String collectionName = "rooms";
-  String? roomId;
-  String? roomName;
-  String? roomDescription;
+  String roomId;
+  String roomName;
+  String roomDescription;
   RoomModel ({required this.roomId, required this.roomName, required this.roomDescription});
   RoomModel.fromJson(Map<String, dynamic> json): this 
       (roomId : json['roomId'] as String,
